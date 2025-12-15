6079bae4f3dfcdab3dd0232f8afb95d3a56c10ec
jialin.yang <870595872@qq.com>
2025-12-15 18:02:00 +0800

    1215记录

--- test.ts
+++ test.ts
@@ -163,7 +163,38 @@ function threeSum(nums: number[]): number[][] {
   //     }
   // }
   // return res;
-  
+  // nums.sort(); // 这是默认字符串排序，不是数字排序
+  nums.sort((a,b) => a-b); // 时间复杂度O(n log n)
+  let res: number[][] = [];
+  for(let i = 0; i < nums.length - 2; i++){
+      if(i > 0 && nums[i] === nums[i-1]){
+          continue;
+      }
+      let left = i+1;
+      let right = nums.length-1;
+      while(left < right){
+          if(left > i+1 && nums[left] === nums[left-1]){
+              left++;
+              continue;
+          }
+          if(right < nums.length-1 && nums[right] === nums[right+1]){
+              right--;
+              continue;
+          }
+          const sum = nums[i]+nums[left]+nums[right]
+          if(sum===0){
+              res.push([nums[i],nums[left],nums[right]])
+              left++;
+              right--;
+          } else if(sum<0){
+              left++;
+          } else {
+              right--;
+          }
+      }
+  }
+  return res;
+
 };
 // 测试函数
 function test() {
